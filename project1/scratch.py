7060565fd86396b0fadc4df4eb0ec403e68efa9f
David Nelson <davidnelson2019@u.northwestern.edu>
2018-02-15 20:28:31 -0600

    method updates

--- project1/scratch.py
+++ project1/scratch.py
@@ -1,11 +1,12 @@
 import json
 import nltk
+from nltk.corpus import *
 import textblob
 from textblob import TextBlob
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
+from alchemyapi import AlchemyAPI
 from pprint import pprint
 
 data = json.load(open('gg2018.json'))
@@ -19,8 +20,12 @@ for tw  in data:
 
 
 #will concatenate to .* or (?!RT) as needed... just used for key words
-patterns = {"best dressed": "((best.*dressed)|(looked.*(great|amazing|beautiful|stylish|handsome))|((beautiful|stylish|fancy|).*outfit))",
+patterns = {"best dressed": "((best.*dressed)|([oO]utfit)|(looked.*(great|amazing|beautiful|stylish|handsome))|((beautiful|stylish|fancy|).*outfit))",
 			"most controversial" : "(([Rr]obbed)|([sS]tole)|([uU]nfair)|([cC]heated)|([Cc]ontroversial)|([wW]rong((ed)?)))" }
+
+junk = "(([gG]olden)|([gG]lobes)|(RT))"
+junkPat = ".*"+junk+".*"
+junkPattern = re.compile(junkPat)
 
 #search finds more tweets but is a lot slower than match
 def tweetSearch(twts, patternKey, keepRTs):
@@ -50,7 +55,7 @@ def getGrams(twts, gramness, pred):
 		grams = blob.ngrams(n=gramness)
 		for gram in grams:
 			for word in gram:
-				if pred(word):
+				if pred(word) and not junkPattern.match(word):
 					text = ""
 					for w in gram:
 						text = text+w.lower()+" "
@@ -64,10 +69,10 @@ def getGrams(twts, gramness, pred):
 	gramList = list()
 	for key,value in gramDict.items():
 		gramList.append((key,value))
-	gramList = sorted(gramList, key=lambda pair: pair[1])
+	gramList = sorted(gramList, key=lambda pair: pair[1], reverse=True)
 	return gramList
 
-print(getGrams(tweetSearch(tweets, "best dressed", True),2, isTitle))
+
 
 #filtering by nltk tag and counting recurrences
 def filtByTag(twts, tag):
@@ -85,10 +90,8 @@ def filtByTag(twts, tag):
 	nameList = list()
 	for key,value in nameDict.items():
 		nameList.append((key,value))
-	nameList = sorted(nameList, key=lambda pair: pair[1])
+	nameList = sorted(nameList, key=lambda pair: pair[1], reverse=True)
 	return nameList	
-
-#print(filtByTag(tweetSearch(tweets,"most controversial", True), "NNP"))
 
 def scrape_awards():
     webUrl = 'https://www.goldenglobes.com/winners-nominees/2018/all'
@@ -121,12 +124,50 @@ def scrape_nominees():
     #print ("\n\n", count)
     return nominees
 
-# Worst Dressed
 
-# Most Talked About
+#helper for findNames
+def extract_entity_names(t):
+	entity_names = []
+	if hasattr(t, 'label') and t.label:
+		if t.label() == 'NE':
+			entity_names.append(' '.join([child[0] for child in t]))
+		else:
+			for child in t:
+				entity_names.extend(extract_entity_names(child))
+	return entity_names
 
-# Most Controversial
+#finds number of occurences of all names in a set of tweets 
+def findNames(tws):
+	nameDict = {}
+	tokenized_sentences = [nltk.word_tokenize(tweet) for tweet in tws]
+	tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+	chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+
+	for tree in chunked_sentences:
+		names = extract_entity_names(tree)
+		for name in names:
+			if not junkPattern.match(name):
+				if name in nameDict:
+					nameDict[name] = nameDict[name]+1
+				else:
+					nameDict[name] = 1
+
+	nameList = list()
+	for key,value in nameDict.items():
+		nameList.append((key,value))
+	nameList = sorted(nameList, key=lambda pair: pair[1], reverse=True)
+	return nameList
 
 
-# print ("\n\n\tAwards\n", scrape_awards())
-# print ("\n\n\tNominees\n", scrape_nominees())
+
+
+#BEST DRESSED
+#print(findNames(tweetSearch(tweets, "best dressed", True))
+
+#most controversial
+ls = findNames(tweetSearch(tweets, "most controversial", True))
+print(ls)
+
+
+
+
